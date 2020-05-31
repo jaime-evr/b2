1f7ad50c36ca29e3ce3784630ca2dbe3c60f1c4f
Jaime Victoria <jaime@michelada.io>
2020-05-30 21:42:46 -0500

    Update b2 test format

--- test/lib/b2_test.rb
+++ test/lib/b2_test.rb
@@ -1,18 +1,23 @@
 # frozen_string_literal: true
 
-require 'test_helper'
+require "test_helper"
 
-class B2Test < Minitest::Test
-  def test_that_it_has_a_version_number
+describe "B2" do
+
+  it "has a version number" do
     refute_nil ::B2::VERSION
   end
 
-  def test_create_client
-    client = B2.create_client do |config|
-      config.app_key = ""
-      config.app_key_id = ""
+  describe "#create_client" do
+    let(:client) do
+      B2.create_client do |config|
+        config.app_key = "API_KEY"
+        config.app_key_id = "API_KEY_ID"
+      end
     end
-    client.authorize_account
-    upload_auth = client.get_upload_url("")
+
+    it "should return an instance of B2" do
+      assert_instance_of B2::Client, client
+    end
   end
 end
